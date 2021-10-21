56eb8319c1832d3ab4c273f73dafa583671061aa
KNK <knkcool2@gmail.com>
2021-10-21 08:59:51 +0530

    imports cleared

--- app/src/main/java/com/nithesh/androidtask/MainActivity.kt
+++ app/src/main/java/com/nithesh/androidtask/MainActivity.kt
@@ -3,9 +3,7 @@ package com.nithesh.androidtask
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.net.NetworkInfo
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -35,16 +33,16 @@ class MainActivity : AppCompatActivity() {
                 Log.i("knk", "onCreate: onclick is clicked")
                 onClick(it)
             }, EmailClickListener {
-                val emailIntent = Intent(Intent.ACTION_SENDTO).apply{
+                val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
                     type = "*/*"
                     data = Uri.parse("mailto:")
                     putExtra(Intent.EXTRA_EMAIL, arrayOf(it))
                     putExtra(Intent.EXTRA_SUBJECT, "Email subject")
                     putExtra(Intent.EXTRA_TEXT, "Email message text")
                 }
-                try{
+                try {
                     startActivity(emailIntent)
-                }catch(e: Exception){
+                } catch (e: Exception) {
                     Toast.makeText(this, "App not found", Toast.LENGTH_SHORT).show()
                 }
             })
@@ -55,11 +53,12 @@ class MainActivity : AppCompatActivity() {
             Log.i("knk", "onCreate: list is submitted ${getEmployeeList()}")
 
 
-        }
-        else{
-            Toast.makeText(this,
+        } else {
+            Toast.makeText(
+                this,
                 "Please check your internet connection",
-                Toast.LENGTH_SHORT).show()
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -87,10 +86,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun isInternetAvailable(): Boolean {
         val connectivityManager = getSystemService(
-            ConnectivityManager::class.java) as ConnectivityManager
+            ConnectivityManager::class.java
+        ) as ConnectivityManager
         val network = connectivityManager.activeNetwork ?: return false
         val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return when{
+        return when {
             activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
             activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
             //for other device how are able to connect with Ethernet
